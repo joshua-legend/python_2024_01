928c7ca72a6f1f74d01a193dc3a4299c4c173628
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-01-03 12:50:20 +0900

    Add files via upload

---
+++ day01/quiz.py
@@ -0,0 +1,8 @@
+name = input("이름 입력:")
+mbti = input("mbti 입력:")
+food = input("최애 음식:")
+print(f"이름은 {name} mbti는 {mbti} 최애 음식은 {food}이군요!😀")
+
+movie = input("최근에 본 영화:")
+ott = input("최근에 본 ott:")
+print(f"최근에 본 영화:{movie} 최근에 본 ott:{ott}")
